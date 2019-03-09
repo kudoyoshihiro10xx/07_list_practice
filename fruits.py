453f31733bb4a8356d1fbf9197bc512423a335cc
Kudo <kudo@example.com>
2019-03-09 22:39:07 +0900

    復習

--- fruits.py
+++ fruits.py
@@ -2,3 +2,8 @@ fruits = ["Apple", "Banana", "Orange", "Grape"]
 
 for fruit in fruits:
     print(fruit)
+
+# for fruit in fruits:
+#     print(f"好きな食べ物は{fruit}です")
+#
+# print(f"好きな食べ物は{fruits[2]}です")
