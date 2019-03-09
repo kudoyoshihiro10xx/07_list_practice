36a1feb921d576352b32a0fe88e9ac485855a003
Kudo <kudo@example.com>
2019-03-09 15:56:38 +0900

    2次元リストの勉強

---
+++ 2d_list.py
@@ -0,0 +1,11 @@
+users = [["Bob", 11],
+         ["Tom", 34],
+         ["Ken", 65]]
+
+# "Bobさんは11歳です"と出力してください
+
+print(f"{users[0][0]}さんは{users[0][1]}歳です")
+print(f"{users[1][0]}さんは{users[1][1]}歳です")
+
+# print(users[0][0])
+# print(users[0][1])
